ffaa530f05e22efb24aaded9d5234d2e0f636a6c
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-04-03 22:47:39 +0900

    spring수업

--- todoList-boot/src/main/java/edu/kh/todo/common/config/DBConfig.java
+++ todoList-boot/src/main/java/edu/kh/todo/common/config/DBConfig.java
@@ -2,10 +2,16 @@ package edu.kh.todo.common.config;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
@@ -30,13 +36,33 @@ import com.zaxxer.hikari.HikariDataSource;
  *   스프링에게 넘기는 어노테이션
  *   (Bean 등록)
  *   
- * DataSource : 
+ * DataSource : Connection 생성 + Connection Pool 지원하는 객체를
+ *              참조하기 위한 Java 인터페이스
+ *              (DriverManager 대안, Java JNDI 기술 이용
+ *              이름만 부르면 오는 거)
+ * 
+ * @Autowired
+ * - 등록된 Bean 중에서
+ *   타입이 일치하거나, 상속관계에 있는 Bean 을
+ *   지정된 필드에 주입
+ *   == 의존성 주입(DI, Dependency Injection, IOC 관련 기술)
  * */
 
 @Configuration
 @PropertySource("classpath:/config.properties")
 public class DBConfig {
-
+	
+	// 필드
+	
+	// org.springframework.context.ApplicationContext;
+	@Autowired
+	private ApplicationContext applicationContext; // application scope 객체 : 즉,현재 프로젝트
+	// DI -> 의존성 주입
+	// => Spring 에 의존함
+	// @Autowired 로 자동 연결
+	
+	// request/session.getContext() application 객체 만들어서 대입했었음
+	
 	// HikariCP 설정
 	
 	// prefix : 접두사
@@ -50,8 +76,8 @@ public class DBConfig {
 		return new HikariConfig();
 	}
 	
-	// 설정 내용들 + CP HikariDataSource 만듦 DataSource interface 에 대입
-	
+	// 설정 내용들 + CP HikariDataSource 만듦 DataSource interface (이미 자바단에서 만들어놓음) 에 대입
+	// CP Connection Pool
 	@Bean
 	public DataSource dataSource(HikariConfig config) {
 		// 위에서 만든 애가 매개변수로 자동 주입
@@ -61,4 +87,65 @@ public class DBConfig {
 		DataSource dataSource = new HikariDataSource(config);
 		return dataSource;
 	}
+	
+	// 위에 까지는 hikariCP 위한 설정
+	
+	// Mybatis 설정
+	
+	public SqlSessionFactory sessionFactory(DataSource dataSource) throws Exception {
+		// 커넥션 + CP + 마이바티스
+		SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
+		
+		// data source 정보 세팅해줘야함
+		// 히카리 데이터 소스를 넣어줌
+		sessionFactoryBean.setDataSource(dataSource);
+		// 다 담아서 공장 객체 만듦
+		
+		// mapper.xml(SQL) 파일이 모이는 경로 지정
+		// -> Mybatis 코드 수행 시 mapper.xml 을 읽을 수 있음
+		// sessionFactoryBean.setMapperLocations("현재프로젝트.자원.어떤파일")
+		// 현재 프로젝트 필드에 application scope 선언해둠
+		// 이미 Spring 이 관리 하는 Bean 임
+		sessionFactoryBean.setMapperLocations(
+				applicationContext.getResources("classpath:/mappers/**.xml") );
+		
+		// 호출하는 애한테 throws Exception
+		
+		// .xml 로 끝나는 모든 파일
+		// todo-mapper.xml SQL 문을 불러서 세팅해준다는 뜻
+		
+		// 해당 패키지 내 모든 클래스의 별칭을 등록 (짧게 부를 수 있도록)
+		// - Mybatis 는 특정 클래스 지정 시 패키지명.클래스명을 모두 작성해야함
+		// -> 너무 길어서 긴 이름을 짧게 부를 별칭 설정할 수 있음
+		
+		// setTypeAliasesPackage("패키지") 이용 시
+		// 클래스 파일명이 별칭으로 등록
+		
+		// ex) edu.kh.todo.model.dto.Todo --> Todo (별칭 등록)
+		sessionFactoryBean.setTypeAliasesPackage("edu.kh.todo");
+		
+		// 마이바티스 설정 파일 경로 지정 (mybatis-config.xml)
+		sessionFactoryBean.setConfigLocation(
+					applicationContext.getResource("classpath:/mybatis-config.xml")
+				);
+		
+		// 설정 내용이 모두 적용된 객체 반환
+		return sessionFactoryBean.getObject();
+	}
+	
+	// SqlSessionTemplate : 기본 SQL 실행 + 트랜잭션 처리
+	/* Connection + DBCP(커넥션 생성하는 거) +Mybatis + 트랜잭션 제어 처리
+	 * 
+	 * */
+	@Bean
+	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sessionFactory) {
+		return new SqlSessionTemplate(sessionFactory);
+	}
+
+	// DataSourceTransactionManager : 트랜잭션 매니저(제어 처리)
+	@Bean
+	public DataSourceTransactionManager dataSourceTransactionManager(DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
+	}
+	
 }
