3725b9218cde41f877766603b1caaee13979dca5
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-04-18 17:46:40 +0900

    보드

---
+++ boardProject-home/src/test/java/com/home/project/BoardProjectHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardProjectHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
