32b472eacfbf5be4698c28611114870a35694859
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-05-21 04:59:16 +0900

    todoTest

---
+++ boardProject-boot/src/main/resources/static/js/todo/todoMain.js
@@ -0,0 +1,17 @@
+console.log("todoMain.js 연결 확인");
+
+const todoAddBtn = document.querySelector("#todo-add-btn");
+
+if(todoAddBtn != null) {
+
+    todoAddBtn.addEventListener("click", () => {
+        
+        // 할 일 추가하기 눌렀을 때 요청자는 자기 자신.
+        // loginMemberNo 를 보내줘야함
+        // 기본 담당자도 자기 자신 선택된 상태
+
+        
+
+    });
+
+};
