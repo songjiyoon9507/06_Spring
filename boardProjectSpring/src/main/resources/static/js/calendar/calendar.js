1dc9b0de1dbd12ce83940c1af84f7f3b04008236
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-06-02 20:43:02 +0900

    full calendar js TEST

--- boardProjectSpring/src/main/resources/static/js/calendar/calendar.js
+++ boardProjectSpring/src/main/resources/static/js/calendar/calendar.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-document.addEventListener('DOMContentLoaded', function() {
-    let draggableEl = document.getElementById('mydraggable');
-=======
 console.log("calendarTest.js 연결 확인");
 
-console.log(loginMemberNo);
-
 document.addEventListener('DOMContentLoaded', function() {
-    // let draggableEl = document.getElementById('mydraggable');
->>>>>>> 4ab28b8215c49c73b8167e012e936e4c7e20a6cc
+
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
@@ -21,44 +14,30 @@ document.addEventListener('DOMContentLoaded', function() {
         timeZone: 'UTC',
         droppable: true,
         dayMaxEvents: true,
-        events: 'https://fullcalendar.io/api/demo-feeds/events.json?overload-day',
+        // events: 'https://fullcalendar.io/api/demo-feeds/events.json?overload-day',
         events: 'https://fullcalendar.io/api/demo-feeds/events.json',
         select: function(info) {
             alert('selected' + info.startStr + ' to ' + info.endStr);
             var title = prompt('Enter event title:');
+            var requesrMember = prompt('일정에 포함된 사람을 입력해주세요.');
             if (title) {
                 calendar.addEvent({
                     title: title,
                     start: info.startStr,
-                    end: info.endStr
+                    end: info.endStr,
+                    description : requesrMember
                 });
-<<<<<<< HEAD
-=======
 
-                
-
->>>>>>> 4ab28b8215c49c73b8167e012e936e4c7e20a6cc
             }
 
+        },
+        eventClick: function(info) {
+            $('#eventModalLabel').text(info.event.title);
+            $('#eventDescription').text(info.event.extendedProps.description);
+            $('#eventModal').modal('show');
         }
     });
 
     calendar.render();
-<<<<<<< HEAD
-    let draggable = new Draggable(draggableEl);
 
-    new Draggable(containerEl, {
-        itemSelector: '.item-class'
-    });
-
-    draggable.destroy();
-=======
-    // let draggable = new Draggable(draggableEl);
-    // new Draggable(containerEl, {
-    //     itemSelector: '.item-class'
-    // });
-    
-    // draggable.destroy();
-    
->>>>>>> 4ab28b8215c49c73b8167e012e936e4c7e20a6cc
 });
